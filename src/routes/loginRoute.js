20687edf85fd5be96ab0e821a9167da729b155a5
Lucas Torquato <lucastorquato@gmail.com>
2021-03-21 18:05:29 -0300

    Implement final login signup and signin

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -2,25 +2,41 @@ const express = require('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 var firebase = require("firebase/app");
-// const controller = require ('../controllers/personController')
+const loginController = require ('../controllers/login.controller')
 
 router.post('/signup', 
   body('email').isEmail(),
   body('password').isLength({ min: 6 }),
+  body('fullname').isLength({ min: 5 }),
+  body('username').isLength({ min: 5 }),
+  body('birthdate').isLength({ min: 10 }),
+  body('state').isLength({ min: 2 }),
+  body('city').isLength({ min: 4 }),
   function (req, res) {
 
     // TODO: Add validator https://express-validator.github.io/docs/
+    // 0. Validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    firebase.auth().createUserWithEmailAndPassword(req.body.email, req.body.password)
+    const { email, password} = req.body;
+
+    // 1. Request on firebase
+    firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((user) => {
-      return res.json({
-        "data": {  
-        }
+
+      // 2. Create on DB
+      loginController.createUser(req.body)
+      .then((result) => {        
+        return res.status(200).json({ data: req.body });
+      })
+      .catch((error) => {
+        console.log('error:' + error);
+        return res.status(400).json({ error: { code: 400, message: 'Insert DB Error'} });
       });
+
     })
     .catch((error) => {
       var errorCode = error.code;
@@ -36,12 +52,19 @@ router.post('/signup',
  });
 
  router.post('/signin', function (req, res) {
+  // 0. Request Firebase
   firebase.auth().signInWithEmailAndPassword(req.body.email, req.body.password)
-    .then((user) => {
-      return res.json({
-        "data": {  
-        }
-      });
+    .then((user) => {   
+      // 1. Get User From DB   
+      loginController.getUserByEmail(user.user.email)
+        .then((result) => {
+          var data = result[0];
+          return res.json({"data": data});
+        })
+        .catch((error) => {
+          return res.status(400).json({ errors: {} });
+        });
+        
     })
     .catch((error) => {
       var errorCode = error.code;
